a878643e9687451874e5bdab019785b5dddb4c43
TalPO24 <talpor212@gmail.com>
2024-02-01 14:49:27 +0200

    Practice React

---
+++ src/components/Arrow Functions.jsx
@@ -0,0 +1,13 @@
+// Arrow Functions
+// Arrow functions allow us to write shorter function syntax:
+
+// Example
+
+hello = function () {
+  return "Hello World!";
+};
+
+// With Arrow Function:
+hello = () => {
+  return "Hello World!";
+};
