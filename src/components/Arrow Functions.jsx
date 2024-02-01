3a0eff38738582c49726bf4ee139c0ecf7268e05
TalPO24 <talpor212@gmail.com>
2024-02-01 15:11:02 +0200

    Arrow Functions

--- src/components/Arrow Functions.jsx
+++ src/components/Arrow Functions.jsx
@@ -11,3 +11,22 @@ hello = function () {
 hello = () => {
   return "Hello World!";
 };
+
+// It get shorter! If the Function has only one statement, and the statment returns a value, you can remove the brackets and the return keyword:
+
+// Example
+// Arrow Functions Return Value by Default:
+hello = () => "Hello world";
+
+//Note: This works only if the function has only one statement.
+
+// If you have parameters, you pass them inside the parentheses:
+
+// Arrow Function With Parameters:
+hello = (val) => "hello" + val;
+
+// In fact, if you have only one parameter, you can skip the parentheses as well:
+
+// Arrow Function Without Parentheses:
+
+hello = (val) => "hello" + val;
