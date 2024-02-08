0d1d0c5ab6808c3cbea0d54255b7be7c6d196e54
TalPO24 <talpor212@gmail.com>
2024-02-08 19:16:13 +0200

    React useCallback Hook

---
+++ src/components/Lesson 3/React useCallback Hook.jsx
@@ -0,0 +1,12 @@
+// React useCallback Hook
+// The React useCallback Hook returns a memoized callback function.
+
+// Think of memoization as caching a value so that it does not need to be recalculated.
+
+// This allows us to isolate resource intensive functions so that they will not automatically run on every render.
+
+// The useCallback Hook only runs when one of its dependencies update.
+
+// This can improve performance.
+
+// The useCallback and useMemo Hooks are similar. The main difference is that useMemo returns a memoized value and useCallback returns a memoized function.
